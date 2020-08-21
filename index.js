f7136bbd0d2e8209256977e1876889af55bc49cb
serim <serim@kono.ai>
2020-08-21 13:55:19 +0900

    add jsonwebtoken

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')  // express 모듈 가져옴
 const app = express()   // 새로운 express app 생성
 const port = 5000
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const config = require('./config/key')
 const { User } = require('./models/User')
@@ -10,7 +11,9 @@ const { User } = require('./models/User')
 app.use(bodyParser.urlencoded({extended: true}));
 // application/json 데이터를 파싱
 app.use(bodyParser.json());
+app.use(cookieParser());
 
+// mongoose로 mongoDB에 연결
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
@@ -28,12 +31,42 @@ app.get('/', (req, res) => {
 // 회원가입 엔드포인트
 app.post('/register', (req, res) => {
     const user = new User(req.body)
+
     user.save((err, userInfo) => {
         if(err) {
             return res.json({ success: false, err })
         }
         return res.status(200).json({
             success: true
+        })
+    })
+})
+
+app.post('/login', (req, res) => {
+    // 요청된 이메일을 db에서 조회
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message : "이메일에 해당하는 유저가 없습니다."
+            })
+        }
+        // 이메일이 존재한다면 비밀번호 일치하는지 검사
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) {
+                return res.json({ 
+                    loginSuccess: false, 
+                    message: "비밀번호가 틀렸습니다."
+                })
+            }
+            // 비밀번호도 일치하다면 토큰 생성
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                // 토큰을 저장(cookie, localStorage 등)
+                return res.cookie("x_auth", user.token)
+                .status(200)
+                .json({ loginSuccess: true, userId: user._id })
+            })
         })
     })
 })
